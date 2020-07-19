56a5499f9864fe3d8e8f9f9cd4534d6cc6f03a22
Kaushal Khokhar <kausha2413@gmail.com>
2020-07-19 19:24:50 +0530

    SVM training

--- ml.py
+++ ml.py
@@ -306,4 +306,66 @@ def applying_knn_to_data():
 
         print('accuracy:', correct/total ) 
 
-applying_knn_to_data()
+# applying_knn_to_data()
+
+class Support_Vector_Machine():
+
+        def __init__(self, visulization=True):
+                self.visulization = visulization
+                self.color = {-1:'r', 1: 'b'}
+                if self.visulization:
+                        self.fig = plt.figure()
+                        self.ax = self.fig.subplot(1,1,1)
+
+        def fit(self, data):
+                self.data = data
+                # || W ||
+                opt_dict = {}
+
+                transform = [ [1, 1],
+                              [-1, 1],
+                              [-1, -1],
+                              [1, -1] ]
+
+                all_data = []
+                for yi in seef.data:
+                        for fetures in self.data[yi]:
+                                for feture in fetures:
+                                        all_data.append(fetures)
+
+                self.max_feature_value = max(all_data)
+                self.min_feature_value = min(all_data)
+                all_data = None
+
+                step_sizes = [self.max_feature_value*0.1,
+                              self.max_feature_value*0.01,
+                              # point of expense
+                              self.max_feature_value*0.001]
+
+                # extremely expensive
+                b_range_multiple = 5
+                #
+                b_multiple = 5
+
+                latest_optimum = self.max_feature_value*10
+
+                for step in step_sizes:
+                        w = np.array([latest_optimum, latest_optimum])
+                        # we can do this because convex
+                        optimized = False
+                        while not optimized:
+                                pass
+        
+        def predict(self, features):
+                # sign(x.w + b)
+                classification = np.sign(np.dot(np.aaray(features), self.w) + self.b)
+
+                return classification
+
+        
+data = {-1:np.array([[1, 5],
+                     [2, 7],
+                     [1, 6],]), 
+          1:np.array([[5, 2],
+                      [6, -1],
+                      [7, 3],])}
