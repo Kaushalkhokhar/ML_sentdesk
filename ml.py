267f25b01254879c24bdb1dd95d0000f291829b6
Kaushal Khokhar <kausha2413@gmail.com>
2020-07-19 18:25:46 +0530

    Applyying knn to real world data

--- ml.py
+++ ml.py
@@ -208,11 +208,11 @@ def KNeighbors_Classifier():
 # KNeighbors_Classifier()
 
 # Euclidean Distance
-from math import sqrt
-import warnings
-from collections import Counter
 
 def KNeighbors_alogorithm():
+        from math import sqrt
+        import warnings
+        from collections import Counter
 
         style.use('fivethirtyeight')
 
@@ -246,11 +246,64 @@ def KNeighbors_alogorithm():
         #         for ii in dataset[i]:
         #                 print(ii)
         #                 plt.scatter(ii[0], ii[1], s=100, color=i)
-
         # or use below
-
         # [[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in i] for i in dataset]
         
         #plt.show()
 
-KNeighbors_alogorithm()
+#KNeighbors_alogorithm()
+
+# Applying KNN to data
+
+def applying_knn_to_data():
+        from math import sqrt
+        import warnings
+        from collections import Counter
+        import random               
+
+        def k_nearest_neighbors(data, predict, k=3):
+                if len(data) >= k:
+                        warnings.warn('K is set ot a value less than total voting group')
+                distances = []
+                for group in data:
+                        for features in data[group]:
+                                euclidean_distance = np.linalg.norm(np.array(features)-np.array(predict)) # this is same as squrt( (point1[0] - point2[0])**2 + (point1[1] - point2[1])**2 )
+                                distances.append([euclidean_distance, group])
+                
+                votes = [i[1] for i in sorted(distances)[:k]]                
+                vote_result = Counter(votes).most_common(1)[0][0]
+                confidence =  Counter(votes).most_common(1)[0][1] / k # Infavour votes out of k
+
+                return vote_result, confidence
+        
+        df = pd.read_csv("breast-cancer-wisconsin.data")
+        df.drop(['id'], 1, inplace=True)
+        df.replace('?', -99999, inplace=True)
+        full_data = df.astype(float).values.tolist()
+        random.shuffle(full_data)
+
+        train_size = 0.2
+        train_set = {2:[], 4:[]}
+        test_set = {2:[], 4:[]}
+        train_data = full_data[:-int(len(full_data)*train_size)] 
+        test_data = full_data[-int(len(full_data)*train_size):]
+
+        for i in train_data:
+                train_set[i[-1]].append(i[:-1])
+
+        for i in test_data:
+                test_set[i[-1]].append(i[:-1])
+
+        correct = 0
+        total = 0
+
+        for group in test_set:
+                for data in test_set[group]:
+                        vote, confidence = k_nearest_neighbors(train_set, data, k=5)
+                        if group == vote:
+                                correct += 1
+                        total += 1
+
+        print('accuracy:', correct/total ) 
+
+applying_knn_to_data()
--- test.py
+++ test.py
@@ -5,16 +5,8 @@ df = {'Low':[1,2,3,4],
       'High':[5,6,7,8]}
 
 df = pd.DataFrame(df)
-#df = df[['Low']]
-df.loc['med'] = [4,5]
-#print(df)
 
-x = [i*2 for i in range(len(df))]
-# print(x)
 
-x =[1, 1, 3, 5, 7, 7]
-
-# print(sorted(x))
-
-print(Counter(x))
-print(Counter(x).most_common())
+for i in df:
+      for data in df[i]:
+            print(data)
